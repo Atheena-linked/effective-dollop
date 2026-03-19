61ad3211e1acce08f4c1b4319d47977f1c02bb27
24f3002331 <24f3002331@ds.study.iitm.ac.in>
2026-03-19 15:07:11 +0530

    Designed and wrote algorithm for dosage and frequency conflicts in medication

---
+++ app/service/conflict_detection.py
@@ -0,0 +1,47 @@
+from collections import defaultdict
+
+def detect_conflicts(records):
+
+    med_map = defaultdict(list)
+
+    for record in records:
+        source = record["source"]
+
+    for med in record["medications"]:
+        name = med["name"].lower()
+        med_map[name].append({
+            "source": source,
+            "dosssage": med["dosage"],
+            "frequency": med["frequency"]
+        })
+
+    conflicts = []
+
+    for med_name, entries in med_map.items():
+
+        dosages = set()
+        frequencies = set()
+
+        for entry in entries:
+            dosage_value = entry["dosage"]
+            frequency_value = entry["frequency"]
+
+            dosages.add(dosage_value)
+            frequencies.add(frequency_value)
+
+        if len(dosages) > 1:
+            conflicts.append({
+                "medication": med_name,
+                "type" : "dosage_conflict",
+                "values" : list(dosages)})
+
+        if len(frequencies) > 1:
+            conflicts.append({
+                "medication": med_name,
+                "type": "frequency_conflict",
+                "values": list(frequencies)
+            })
+
+    return conflicts
+
+    
