c2496cd2e5d739b56f5de99dce711bf069654738
24f3002331 <24f3002331@ds.study.iitm.ac.in>
2026-03-19 13:07:19 +0530

    Added GET API to fetch medication records by patient_id

--- app/routes/medication_routes.py
+++ app/routes/medication_routes.py
@@ -9,3 +9,20 @@ async def add_medication_record(record:MedicationRecord):
     await medication_collection.insert_one(record.model_dump())
     return {"message": "Record stored"}
 
+
+
+@router.get("/medications/{patient_id}")
+async def get_medication_records(patient_id: str):
+
+    records = []
+
+    cursor = medication_collection.find({"patient_id":patient_id})
+
+    async for doc in cursor:
+        doc["_id"] = str(doc["_id"])
+        records.append(doc)
+
+    return {
+        "patient_id": patient_id,
+        "records": records
+    }
