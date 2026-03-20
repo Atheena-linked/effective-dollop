563b53be0849745acce7b189f0a32c304ab12fee
24f3002331 <24f3002331@ds.study.iitm.ac.in>
2026-03-20 16:01:37 +0530

    add endpoint to count patients with ≥2 conflicts per clinic in last 30 days and a temporary test  data file to seed data to test the endpoint

--- app/routes/analytics.py
+++ app/routes/analytics.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from app.config.database import conflict_collection
+from datetime import datetime, timedelta
 
 router = APIRouter()
 
@@ -31,3 +32,47 @@ async def get_patients_with_high_conflicts():
         })
 
     return {"patients": results}
+
+
+@router.get("/analytics/conflicts-per-clinic")
+async def conflicts_per_clinic():
+
+    last_30_days = datetime.utcnow() - timedelta(days=30)
+
+
+    pipeline = [
+        {
+            "$match": {
+                "timestamp": {"$gte": last_30_days}
+            }
+        },
+        {
+            "$group": {
+                "_id": {
+                    "clinic": "$source",
+                    "patient": "$patient_id"
+                },
+                "conflict_count": {"$sum": 1}
+            }
+        },
+        {
+            "$match": {
+                "conflict_count": {"$gte": 2}
+            }
+        },
+        {
+            "$group": {
+                "_id": "$_id.clinic",
+                "patients_with_conflicts": {"$sum": 1}
+            }
+        }
+    ]
+
+    result = []
+
+    cursor = conflict_collection.aggregate(pipeline)
+
+    async for doc in cursor:
+        result.append(doc)
+
+    return result
