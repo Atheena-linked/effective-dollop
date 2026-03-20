0d1126c5404f9cf7eaafd8d09f8a50b46912efd7
24f3002331 <24f3002331@ds.study.iitm.ac.in>
2026-03-20 14:17:15 +0530

    feat: add endpoint to fetch patients with more than 2 conflicts using MongoDB aggregation

---
+++ app/routes/analytics.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter
+from app.config.database import conflict_collection
+
+router = APIRouter()
+
+@router.get("/patients/high-conflicts")
+async def get_patients_with_high_conflicts():
+
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$patient_id",
+                "conflict_count": {"$sum": 1}
+            }
+        },
+        {
+            "$match": {
+                "conflict_count": {"$gt": 2}
+            }
+        }
+    ]
+
+    results = []
+
+    cursor = conflict_collection.aggregate(pipeline)
+
+    async for doc in cursor:
+        results.append({
+            "patient_id": doc["_id"],
+            "conflict_count": doc["conflict_count"]
+        })
+
+    return {"patients": results}
