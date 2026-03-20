96c43eaf6931a39f41351d6cad40004cd1328213
24f3002331 <24f3002331@ds.study.iitm.ac.in>
2026-03-20 19:07:02 +0530

    chore: move config to environment variables

--- app/config/database.py
+++ app/config/database.py
@@ -1,10 +1,15 @@
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+import os
 
-MONGO_URL = "mongodb://localhost:27017"
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+DB_NAME = os.getenv("DB_NAME", "medication_db")
 
 client = AsyncIOMotorClient(MONGO_URL)
 
-database = client.medication_db
+database = client[DB_NAME]
 
 medication_collection = database.get_collection("medications")
 conflict_collection = database.get_collection("conflicts")
